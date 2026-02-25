ef087cce2fc93c9e470ccb56c68070b884ee5606
Yamile0712 <yamileariasperez07@gmail.com>
2026-02-25 11:26:24 -0500

    versión 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Header} from './components/Header.jsx'
+import { Home } from './Pages/Home'
+
+export const App = () => {
+
+  return (
+    <>
+    <Header />
+    <Home />
+     </>
+    
+  )
+}
+
