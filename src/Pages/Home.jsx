09f72ab609b9fa179d88dc4361f559c262bccfeb
Yamile0712 <yamileariasperez07@gmail.com>
2026-02-26 11:22:35 -0500

    se agrego la informacion

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,39 +1,40 @@
+import "../stylesheets/Home.css"
 export const Home = () => {
     return (
         <>
-            <div className="container text-center">
-                <h1>Row 1</h1>
+            <div className="container">
                 <div className="row">
-                    <div className="col-6 col-md-4 col-lg-3 border">
-                        Column
-                    </div>
-                    <div className="col-6 col-md-4 col-lg-3  border">
-                        Column
-                    </div>
-                    <div className="col-6 col-md-4 col-lg-3 border">
-                        Column
-                    </div>
-                    <div className="col-6 col-md-4 col-lg-3  border">
-                        Column
-                    </div>
-                </div>
+                    <div className="col-6 ">
+                        <div className="d-flex flex-column justify-content-center h-100 gap-5">
+                            <div>
 
+                                <h1>Make <br />remote work</h1>
 
-                <h1>Row 2</h1>
-                <div className="row">
-                    <div className="col-6 border">
-                        Column
-                    </div>
-                    <div className="col-6 border">
-                        Column
-                    </div>
-                    <div className="col-6 border">
-                        Column
-                    </div>
-                    <div className="col-6 border">
-                        Column
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus reprehenderit quisquam doloremque fugit perspiciatis magnam possimus reiciendis quam maxime corporis </p>
+
+                                <a href="http://www.jango.com" className="btn btn-dark">learn more</a>
+
+                            </div>
+
+                            <div className="d-flex gap-3 align-items-center">
+                            
+                                <img src="./shopify.svg" height="25" alt="" />
+                                <img src="./openai.svg" height="25" alt="" />
+                                <img src="./wiz.svg" height="25" alt="" />
+                                <img src="./linear.svg" height="25" alt="" />
+
+                            </div>
+
+                        </div>
                     </div>
 
+                    <div className="col-6 ">
+
+                        <div className="">
+                            <img src="./hongo.webp" className="img-cropped" alt="" />
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </>
